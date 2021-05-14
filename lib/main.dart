43bc97aade70e83f673ced9e44ffbeb02e1dac75
wantek13 <h_core57@outlook.fr>
2021-05-14 22:58:26 +0200

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,71 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key key}) : super(key: key);
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+
+  String value = '';
+
+  void onShow(){
+    setState(() {
+      value = new DateTime.now().toString();
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Floating Button"),
+      ),
+      // floating action Button here
+      body: Column(
+        children: <Widget> [
+          Text(value,
+            style: TextStyle(fontSize: 40, color: Colors.blue),
+            textAlign: TextAlign.center,)
+        ],
+      ),
+
+      // floatingActionButton: FloatingActionButton(
+      //   onPressed: onShow,
+      //   backgroundColor: Colors.red,
+      //   child: Icon(Icons.timer),
+      // ),
+
+      floatingActionButton: FloatingActionButton.extended(
+        onPressed: onShow,
+        backgroundColor: Colors.red,
+        label: Text('Time'),
+        icon: Icon(Icons.timer),
+      ),
+
+    );
+  }
+}
+
+
+
